4c9484a854688884c0f63f977c88e7f79ac126fa
juan <juanrm071299@gmail.com>
2021-09-20 22:55:00 -0600

    implementacion del metodo crearSocket

--- Server/SocketServer.cpp
+++ Server/SocketServer.cpp
@@ -5,8 +5,10 @@ SocketServer::SocketServer() {}
 bool SocketServer::crearSocket() {
     //Crear un descriptor
     descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
     if (descriptor < 0) //valida la creacion del socket
         return false;
+    
     info.sin_family = AF_INET; //IPV4 tipo de conexion
     info.sin_addr.s_addr = INADDR_ANY; //acepta a cualquiera con mi ip y mi puerto
     info.sin_port = htons(4050); //definimos el puerto
