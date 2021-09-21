c37fb4eb67b2427534f83b833061aae350392620
juan <juanrm071299@gmail.com>
2021-09-20 22:55:32 -0600

    Implementacion del metodo enlazarKernel

--- Server/SocketServer.cpp
+++ Server/SocketServer.cpp
@@ -8,7 +8,7 @@ bool SocketServer::crearSocket() {
 
     if (descriptor < 0) //valida la creacion del socket
         return false;
-    
+
     info.sin_family = AF_INET; //IPV4 tipo de conexion
     info.sin_addr.s_addr = INADDR_ANY; //acepta a cualquiera con mi ip y mi puerto
     info.sin_port = htons(4050); //definimos el puerto
@@ -19,6 +19,7 @@ bool SocketServer::crearSocket() {
 bool SocketServer::enlazarKernel() {
     if ((bind(descriptor, (sockaddr *)&info, (socklen_t)sizeof(info))) < 0)//enlaza al socket con el kernel
     return false;
+
     // escuchar a los clientes
     listen(descriptor,4);
     return true;
